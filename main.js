31995bef2e8f85524459e677d0536eb43f2e842a
Swolebrain <vic574m@gmail.com>
2016-03-08 19:46:41 -0500

    Made code better

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
 $(document).ready(function(){
-    var cnote = document.getElementById("c-note");
-    var dnote = document.getElementById("d-note");
-    var enote = document.getElementById("e-note");
-    var fnote = document.getElementById("f-note");
-    var gnote = document.getElementById("g-note");
-    var anote = document.getElementById("a-note");
-    var bnote = document.getElementById("b-note");
-    $("#c").on("click mouseover", function(){
-        cnote.currentTime =0;
-        cnote.play();
+  var notes = {
+    c:  document.getElementById("c-note"),
+    d:  document.getElementById("d-note"),
+    e:  document.getElementById("e-note"),
+    f:  document.getElementById("f-note"),
+    g:  document.getElementById("g-note"),
+    a:  document.getElementById("a-note"),
+    b: document.getElementById("b-note")
+  };
+    $("#instrument").find("div").on("click mouseover", 
+                              function(){
+      console.log($(this).attr("id"));
+      play($(this).attr("id"));
     });
     
-    $("#d").on("click mouseover", function(){
-        dnote.currentTime = 0;
-        dnote.play();
+    //Question 1: 
+    $(document).on("keypress", function(evt){
+      console.log(evt.keyCode);
+        if (evt.keyCode === 122) play("c");
+        else if (evt.keyCode === 120) play("d");
+        else if (evt.keyCode === 99) play("e");
+        else if (evt.keyCode === 118) play("f");
+        else if (evt.keyCode === 98) play("g");
+        else if (evt.keyCode === 110) play("a");
+        else if (evt.keyCode === 109) play("b");
+        
     });
+    function play(x){
+      notes[x].currentTime = 0;
+      notes[x].play();
+    }
     
-    $("#e").on("click mouseover", function(){
-        enote.currentTime = 0;
-        enote.play();
-    });
-    
-    $("#f").on("click mouseover", function(){
-        fnote.currentTime =0;
-        fnote.play();
-    });
-    
-    $("#g").on("click mouseover", function(){
-        gnote.currentTime = 0;
-        gnote.play();
-    });
-    
-    $("#a").on("click mouseover", function(){
-        anote.currentTime = 0;
-        anote.play();
-    });
-    
-    $("#b").on("click mouseover", function(){
-        bnote.currentTime = 0;
-        bnote.play();
-    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
